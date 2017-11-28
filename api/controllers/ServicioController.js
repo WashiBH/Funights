787bddb7671bd317780a82936fd5a43959015816
WashiBH <washi1886@gmail.com>
2017-11-28 10:49:23 -0500

    create model Local

---
+++ api/controllers/ServicioController.js
@@ -0,0 +1,11 @@
+/**
+ * ServicioController
+ *
+ * @description :: Server-side logic for managing Servicios
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Local.js
@@ -0,0 +1,83 @@
+/**
+ * Local.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      required: true
+    },
+    nombre:{
+      type:"string",
+      required: true
+    },
+    direccion:{
+      type:"string",
+      required: true
+    },
+    longitud:{
+      type:"string",
+      required: true
+    },
+    latitud:{
+      type:"string",
+      required: true
+    },
+    estado:{
+      type:"boolean",
+      required: true
+    },
+    tipo:{
+      type:"array",
+      required: true
+    },
+    horario:{
+      type:"string",
+      required: true
+    },
+    fotos:{
+      type:"array",
+      required: true
+    },
+    responsable:{
+      type:"string",
+      required: true
+    },
+    descripcion:{
+      type:"text",
+      required: true
+    },
+    razon_social:{
+      type:"string",
+      required: true
+    },
+    telefono:{
+      type:"string",
+      required: true
+    },
+    likes:{
+      type:"integer",
+      required: true
+    },
+    comentarios:{
+      type:"array",
+      required: true
+    },
+    ciudad:{
+      type:"string",
+      required: true
+    },
+    pais:{
+      type:"string",
+      required: true
+    }
+  },
+  connection:"mongodb"
+};
+
